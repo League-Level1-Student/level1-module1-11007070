6612dd3a36c3e956919f0eaf3ebb9a060a653279
root <root@1906a4627116>
2021-07-14 23:58:37 +0000

    7/14/21

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,14 +11,22 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
+		RaceCar car = new RaceCar("lambourghini", 5);
 	
 		// 2. Print the RaceCar's position in the race
-
+		System.out.println("Your " + car.getBrand() + " race car is in position number " + car.getPositionInRace() + ".");
+		
 		// 3. Crash the RaceCar
+		car.crash();
 		
 		// 4. If the car is damaged. Bring it in for a pit stop.
+		if (car.damaged == true) {
+			car.pit();
+		}
 
 		// 5. Help the car move into first place.
-
+		for (int i = 0; i < 7; i++) {
+			car.overtake();
+		}
 	}
 }
---
+++ src/_03_harry_potter/HarryPotterRunner.java
@@ -0,0 +1,8 @@
+package _03_harry_potter;
+
+public class HarryPotterRunner {
+	public static void main(String[] args) {
+		HarryPotter harry = new HarryPotter();
+		
+	}
+}
